8bda70491406db0a7dc47d6a2340fe2bdda0dc42
Rizki Oktavia Ningrum <rizkioktavianingrum@gmail.com>
2019-08-04 00:22:18 +0700

    Initial commit

---
+++ myviewkotlinverse/src/main/java/net/roningrum/myviewkotlinverse/MainActivity.kt
@@ -0,0 +1,18 @@
+package net.roningrum.myviewkotlinverse
+
+import android.drm.DrmStore
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.ActionBar
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if(supportActionBar != null){
+            (supportActionBar as ActionBar).title = "Google Pixel"
+        }
+    }
+}
